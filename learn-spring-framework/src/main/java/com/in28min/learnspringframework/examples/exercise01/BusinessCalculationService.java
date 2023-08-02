9a65a70372f9f8513314b11e6c071bf7946557b1
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2023-08-02 20:42:14 +0300

    Used different stereotype annotations

--- learn-spring-framework/src/main/java/com/in28min/learnspringframework/examples/exercise01/BusinessCalculationService.java
+++ learn-spring-framework/src/main/java/com/in28min/learnspringframework/examples/exercise01/BusinessCalculationService.java
@@ -1,10 +1,10 @@
 package com.in28min.learnspringframework.examples.exercise01;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 
-@Component
+@Service
 public class BusinessCalculationService {
     private final DataService dataService;
 
--- learn-spring-framework/src/main/java/com/in28min/learnspringframework/examples/exercise01/MySQLDataService.java
+++ learn-spring-framework/src/main/java/com/in28min/learnspringframework/examples/exercise01/MySQLDataService.java
@@ -1,8 +1,8 @@
 package com.in28min.learnspringframework.examples.exercise01;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class MySQLDataService implements DataService {
     @Override
     public int[] retrieveData() {
